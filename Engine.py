54918665c57ce52cb103e1b2c15d92dde3fd9824
Vardiak <contact@vardiak.fr>
2018-03-23 12:56:27 +0100

    :construction: Cube & map model

--- Engine.py
+++ Engine.py
@@ -9,7 +9,7 @@ import math
 
 class Camera:
     x = 0
-    y = 0
+    y = 2
     z = -5
 
 class Engine:
@@ -22,9 +22,6 @@ class Engine:
 
         self.root = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.root, width=width, height=height, background='white')
-
-        self.lastFrame = time.clock()
-
 
         # temporary
         self.points = [
@@ -39,11 +36,18 @@ class Engine:
         ]
 
         self.faces = [
-            [0, 1, 2],
-            [1, 2, 3],
-            [4, 5, 6],
-            [5, 6, 7]
+            [0, 1, 3, 2],
+            [4, 5, 7, 6],
+            [1, 5, 4, 0],
+            [2, 3, 7, 6]
+
+
+            # [0, 1, 2],
+            # [1, 2, 3],
+            # [4, 5, 6],
+            # [5, 6, 7]
         ]
+        self.map = Map()
         
         self.mainloop()
 
@@ -77,18 +81,21 @@ class Engine:
                 fps = self.targetFPS / (time.clock() - cycleStart)
                 cycleStart = time.clock()
 
+            self.cam.x += 0.05
+            self.cam.y += 0.05
+
 
 
     def render(self):
         self.canvas.delete('all')
 
-        for face in self.faces:
+        for face in self.map.render():
 
-            coords = [];
+            coords = []
 
-            for pointId in face:
+            for (x, y, z) in face:
 
-                (x, y, z) = self.points[pointId]
+                # (x, y, z) = self.points[pointId]
 
                 # Camera position
                 x -= self.cam.x
@@ -97,10 +104,11 @@ class Engine:
 
 
                 # Projection
-                f = (self.height / 2) / z
+                f = 1 / z
+                f = (self.width / 2) / z
 
                 x = x * f + (self.width / 2)
-                y = y * f + (self.height / 2)
+                y = - y * f + (self.height / 2)
                 coords.append((x, y))
 
             # print(coords)
@@ -111,3 +119,54 @@ class Engine:
                     self.canvas.create_line(coords[i] + coords[i + 1])
                 else:
                     self.canvas.create_line(coords[i] + coords[0])
+
+
+
+class Map:
+
+    def __init__(self):
+        self.objects = []
+
+        for i in range(10):
+            for j in range(10):
+                for k in range(1):
+                    self.objects.append(Cube(i, j , k, 1))
+        self.results = []
+
+    def render(self):
+        if self.results:
+            return self.results
+
+        faces = []
+        for i in self.objects:
+            faces += i.render()
+
+        self.results = faces
+        return faces
+
+
+class Cube:
+
+    def __init__(self, x, y, z, size):
+        self.pos = (x, y, z)
+        self.size = size
+
+    def render(self):
+        (x, y, z) = self.pos
+        points = [
+            (x, y, z),
+            (x, y, z + self.size),
+            (x, y + self.size, z),
+            (x, y + self.size, z + self.size),
+            (x + self.size, y, z),
+            (x + self.size, y, z + self.size),
+            (x + self.size, y + self.size, z),
+            (x + self.size, y + self.size, z + self.size),
+        ]
+
+        return [
+            [points[0], points[1], points[3], points[2]],
+            [points[4], points[5], points[7], points[6]],
+            [points[3], points[7], points[5], points[1]],
+            [points[0], points[2], points[6], points[4]]
+        ]
